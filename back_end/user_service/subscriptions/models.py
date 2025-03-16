68a180f766aed817000ae8f99c0ca3514eeff026
petrykdimaajax <petryk.d@ajax.systems>
2025-03-16 20:10:12 +0200

    Added max_length to Subscription.description TextField

--- back_end/user_service/subscriptions/models.py
+++ back_end/user_service/subscriptions/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Subscription(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(max_length=256)
     subscribed_at = models.DateTimeField(auto_now_add=True)
     payment_sum = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3, default='USD')
