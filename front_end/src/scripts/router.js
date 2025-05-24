29c533d1591daa7d9ee2e9af9c23df368a5b2592
Oleksandr Samus <samus.o.v@ajax.systems>
2025-05-24 17:51:24 +0300

    added cards page

--- front_end/src/scripts/router.js
+++ front_end/src/scripts/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/Login.vue';
 import Register from '../components/Register.vue';
 import Home from '../components/Home.vue';
+import Cards from '../components/Cards.vue';
 
 const routes = [
     { path: '/login', component: Login },
     { path: '/register', component: Register },
-    { path: '/home', component: Home }
+    { path: '/home', component: Home },
+    { path: '/cards', component: Cards }
 ];
 
 const router = createRouter({
