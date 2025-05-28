0deb7a931fc2318c35f9d11dbaa7a83dd1ea1cd5
Oleksandr Samus <samus.o.v@ajax.systems>
2025-05-28 22:46:42 +0300

    fix

--- back_end/user_service/banking/views.py
+++ back_end/user_service/banking/views.py
@@ -2,9 +2,11 @@ import time
 from datetime import datetime
 
 import requests
+from django.core.cache import cache
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .utils.categorizer import Categorizer
 
 
 class MonobankPersonalInfoListView(APIView):
@@ -12,29 +14,30 @@ class MonobankPersonalInfoListView(APIView):
 
     def get(self, request, *args, **kwargs):
         try:
-            if not request.user.monobank_token:
-                return Response({"error": "Monobank token not found"}, status=400)
-            raw_token = request.user.monobank_token
+            cache_key = f'monobank_client_info_{request.user.id}'
+            ttl_seconds = 60
+            if cached_data := cache.get(cache_key):
+                data = cached_data
+            else:
+                if not request.user.monobank_token:
+                    return Response({"error": "Monobank token not found"}, status=400)
+                raw_token = request.user.monobank_token
 
-            if isinstance(raw_token, bytes):
-                raw_token = raw_token.decode("utf-8")
+                if isinstance(raw_token, bytes):
+                    raw_token = raw_token.decode("utf-8")
 
-            token = raw_token.strip()
-            print('token: ', token)
-            monobank_response = requests.get(
-                "https://api.monobank.ua/personal/client-info",
-                headers={"X-Token": token},
-            )
-            monobank_response = requests.get(
-                "https://api.monobank.ua/personal/client-info",
-                headers={"X-Token": token},
-            )
-            print('monobank response: ', monobank_response)
-            monobank_response.raise_for_status()
-            return Response(monobank_response.json())
+                token = raw_token.strip()
+                response = requests.get(
+                    "https://api.monobank.ua/personal/client-info",
+                    headers={"X-Token": token},
+                )
+                response.raise_for_status()
+                data = response.json()
+                cache.set(cache_key, data, ttl_seconds)
+            return Response(data)
         except requests.exceptions.RequestException:
             return Response({"error": f"Failed to fetch Monobank data\n"
-                                      f"monobank response: {monobank_response}\n"
+                                      f"monobank response: {response}\n"
                                       f"mono token: {request.user.monobank_token}"}, status=502)
 
 
@@ -61,12 +64,23 @@ class MonobankPersonalInfoFromToListView(APIView):
         url = f"https://api.monobank.ua/personal/statement/{account}/{ts_from}/{ts_to}"
 
         try:
-            response = requests.get(
-                url,
-                headers={"X-Token": token},
-                timeout=5
-            )
-            response.raise_for_status()
-            return Response(response.json())
+            cache_key = f'monobank_statement_{request.user.id}_{account}_{ts_from}_{ts_to}'
+            ttl_seconds = 60
+            if cached_data := cache.get(cache_key):
+                data = cached_data
+            else:
+                response = requests.get(
+                    url,
+                    headers={"X-Token": token},
+                    timeout=5
+                )
+                response.raise_for_status()
+                data = response.json()
+                cache.set(cache_key, data, ttl_seconds)
+
+            if request.query_params.get("category"):
+                data = Categorizer.categorize(data)
+            return Response(data)
+
         except requests.exceptions.RequestException as e:
             return Response({"error": f"Failed to fetch statement: {str(e)}"}, status=502)
